6be62ae38d252b7ee126b54062a357a7d913c5e1
LucasOlivel <olivel890@gmail.com>
2020-02-28 00:41:11 -0300

    New Flags/Foods

--- app/src/main/java/br/com/bossini/exercicio1402/MainActivity.java
+++ app/src/main/java/br/com/bossini/exercicio1402/MainActivity.java
@@ -3,6 +3,7 @@ package br.com.bossini.exercicio1402;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Versão final", Toast.LENGTH_SHORT).show();
     }
-    //Autor
+
 }
